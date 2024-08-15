4b58a9d5fcb31fb86a1b3db74cc8025f442e7b17
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-08-15 15:56:56 +0200

    Création du component LoginPage

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const LoginPage = () => {
+
+    return (
+        <main className="main bg-dark">
+            <section className="sign-in-content">
+                <i className="fa fa-user-circle sign-in-icon"></i>
+                <h1>Sign In</h1>
+                <form>
+                    <div className="input-wrapper">
+                        <label htmlFor="email">Username</label>
+                        <input type="email" id="email" required />
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="password">Password</label
+                        ><input type="password" id="password" required />
+                    </div>
+                    <div className="input-remember">
+                        <input type="checkbox" id="remember-me" />
+                        <label htmlFor="remember-me">Remember me</label>
+                    </div>
+
+                    <button type="submit" className="sign-in-button">Sign in</button>
+                </form>
+            </section>
+        </main>
+    );
+};
+
+export default LoginPage;
