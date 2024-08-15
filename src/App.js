fa5a0c0fde6c81e2c898b79897e0613bf7d725f7
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-08-15 14:02:01 +0200

    Ajout de HomePage dans App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import HomePage from './pages/HomePage';
 import './App.css';
 
 function App() {
   return (
     <div>
         <Header />
+        <HomePage />
     </div>
   );
 }
