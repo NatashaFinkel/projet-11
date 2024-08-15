66ea9228d5ad60034a77f9ccd59dea85885dd84d
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-08-15 15:58:37 +0200

    Importation de AppRouter dans App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import Header from './components/Header';
-import HomePage from './pages/HomePage';
+import AppRouter from './components/AppRouter';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Header />
-      <HomePage />
+      <AppRouter />
       <Footer />
     </div>
   );
