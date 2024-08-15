6c90123867bd6b49e816d1eb6ef0d8ddd580ba30
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-08-15 16:50:13 +0200

    Utilisation du composant Button dans ProfilePage

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Account from "../components/Account";
+import Button from "../components/Button";
 
 function ProfilePage() {
     return (
         <main className="main bg-dark">
             <div className="header">
                 <h1>Welcome back<br />Tony Jarvis!</h1>
-                <button className="edit-button">Edit Name</button>
+                <Button btnClassName="edit-button"  btnTxt="Edit Name"  />
             </div>
-            
+
             <h2 className="sr-only">Accounts</h2>
             <Account title="Argent Bank Checking (x8349)" amount="$2,082.79" description="Available Balance" />
             <Account title="Argent Bank Savings (x6712)" amount="$10,928.42" description="Available Balance" />
