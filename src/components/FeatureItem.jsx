579187131b81675c282f66d962725c0701ef17b3
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-08-15 13:58:20 +0200

    Création du composant FeatureItem

---
+++ src/components/FeatureItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "./Image";
+
+function FeatureItem({ imgSrc, imgAlt, featureItemTitle, featureItemP }) {
+    return (
+        <div className="feature-item">
+            <Image imgClassName="feature-icon" imgSrc={imgSrc} imgAlt={imgAlt} />
+            <h3 className="feature-item-title">{featureItemTitle}</h3>
+            <p>{featureItemP}</p>
+        </div>
+    )
+}
+
+export default FeatureItem;
