2ec4dee0203c678c879700d9f9a9d3e1f1e01e82
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-08-15 14:05:10 +0200

    Ajout des deux derniers FeatureItem de la page HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import FeatureItem from "../components/FeatureItem";
 import iconChatImg from "../img/icon-chat.png";
+import iconMoneyImg from "../img/icon-money.png";
+import iconSecurityImg from "../img/icon-security.png";
 
 function HomePage() {
     return (
@@ -17,9 +19,11 @@ function HomePage() {
 
             <section className="features">
                 <h2 className="sr-only">Features</h2>
-
                 <FeatureItem imgSrc={iconChatImg} imgAlt="Chat Icon" featureItemTitle="You are our #1 priority" featureItemP=" Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes." />
 
+                <FeatureItem imgSrc={iconMoneyImg} imgAlt="money Icon" featureItemTitle="More savings means higher rates" featureItemP="The more you save with us, the higher your interest rate will be!" />
+
+                <FeatureItem imgSrc={iconSecurityImg} imgAlt="Security Icon" featureItemTitle="Security you can trust" featureItemP="We use top of the line encryption to make sure your data and money is always safe." />
             </section>
         </div>
     )
