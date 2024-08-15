f65679ea88302339bbbba8236d33dcd0f2d7ff27
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-08-15 13:49:33 +0200

    Création des composants Header et Image

---
+++ src/components/Image.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Image({imgClassName, imgSrc, imgAlt}) {
+    return (
+        <img className={imgClassName}
+            src={imgSrc}
+            alt={imgAlt}
+        />
+    )
+}
+
+export default Image;
