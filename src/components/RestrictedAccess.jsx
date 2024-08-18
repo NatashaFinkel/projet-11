22a6c9d299d4635aa6c2cbedf932b9952518be44
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-08-18 17:41:33 +0200

    Ajout de commentaires au composant RestrictedAccess

--- src/components/RestrictedAccess.jsx
+++ src/components/RestrictedAccess.jsx
@@ -4,6 +4,10 @@ import { useSelector } from "react-redux";
 
 const RestrictedAccess = () => {
     const token = useSelector(state => state.authentication.token);
+
+    //  Si le token existe ( = utilisateur authentifié),
+    //  le composant Outlet ( = la route dont l'accès est protégé) est restitué.
+    //  Si le token n'existe pas, l'utilisateur est automatiquement redirigé vers la page login.
     return token ? <Outlet /> : <Navigate to="/login" />
 }
 
