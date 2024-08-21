1431cf7d374a2a2f1cac256084ef7d79b6e4e48c
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-08-21 16:27:44 +0200

    Clean code JavaScript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
