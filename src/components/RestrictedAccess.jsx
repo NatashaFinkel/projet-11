f50c1a244fa53acf4ac500196e9965bf19f2db4f
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-08-25 12:30:01 +0200

    Refactorisation des imports de RestrictedAccess.jsx

--- src/components/RestrictedAccess.jsx
+++ src/components/RestrictedAccess.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
 
 const RestrictedAccess = () => {
     const token = useSelector(state => state.authentication.token);
