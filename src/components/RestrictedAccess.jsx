f97f6fd17086ce9589a479ab3a7bb55c9468abe6
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2025-01-28 06:29:05 +0100

    Fix : modification de la redirection dans RestrictedAccess.jsx

--- src/components/RestrictedAccess.jsx
+++ src/components/RestrictedAccess.jsx
@@ -5,11 +5,14 @@ import { Navigate, Outlet } from "react-router-dom";
 const RestrictedAccess = () => {
     const token = useSelector(state => state.authentication.token);
 
+    const appBasename = process.env.PUBLIC_URL;
+
     // if there is a token ( = authenticated user),
     // the component Outlet ( = the route whose access is restricted) is rendered.
     // If there is no token, the user is automatically redirected to the login page.
 
-    return token ? <Outlet /> : <Navigate to="/login" />
+
+    return token ? <Outlet /> : <Navigate to={appBasename + "/login"} />
 }
 
 export default RestrictedAccess;
